b3f450cb50dddb5b27cd267975118c3f197af66b
dtg01100 <dlafreniere349@gmail.com>
2017-04-20 15:57:36 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import tkinter
+import tkinter.ttk
+import time
+import threading
+import ping_lib
+
+
+class PingLooper(object):
+    def __init__(self):
+        self.running = False
+        self.delay = "5"
+        self.net_address = "127.0.0.1"
+        self.log_file_path = ""
+
+    def _looper_logic(self):
+        self.running = True
+        ping_fire = False
+        ping_fire_counter = 0
+        while self.running:
+            ping_fire_counter += 1
+            if ping_fire_counter == int(self.delay) * 60:
+                ping_fire = True
+            counter = 1
+            if ping_fire:
+                print("test" + str(counter))
+                ping_fire = False
+                ping_fire_counter = 0
+            else:
+                print("dead_loop_test" + str(ping_fire_counter))
+            counter += 1
+            time.sleep(1)
+
+    def run(self):
+        self._looper_logic()
+
+looper_test = PingLooper()
+looper_test.delay = 1
+looper_test.run()
