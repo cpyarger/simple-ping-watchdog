4a7ffe98f3c2094ffeb4ac0c98b91aa5f5191dd5
dtg01100 <dlafreniere349@gmail.com>
2017-05-01 14:24:21 -0400

    fix an annoying pep8 linter issue

--- ping_lib.py
+++ ping_lib.py
@@ -1,4 +1,4 @@
-from platform import system as system_name # Returns the system/OS name
+from platform import system as system_name  # Returns the system/OS name
 from os import system as system_call       # Execute a shell command
 
 
